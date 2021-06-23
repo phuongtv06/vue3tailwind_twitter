3d6eb49ca73d97bf8c9c75c9d99e757c20aeb0ec
phuongtv06 <06hthang6@gmail.com>
2021-06-23 08:31:34 +0700

    push

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 import './assets/css/tailwind.css'
-import './assets/css/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'; 
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(store).use(router).mount('#app')
